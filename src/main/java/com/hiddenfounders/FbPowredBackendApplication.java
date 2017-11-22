eafdd1ba51cb00718c2121c0889d95d00667d681
younes <youness.ezzarouali@gmail.com>
2017-11-22 00:28:32 +0000

    first commit

---
+++ src/main/java/com/hiddenfounders/FbPowredBackendApplication.java
@@ -0,0 +1,12 @@
+package com.hiddenfounders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbPowredBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbPowredBackendApplication.class, args);
+	}
+}
